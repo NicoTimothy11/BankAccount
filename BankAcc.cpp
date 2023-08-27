8e6da3976e0a629e30b69d450659324e2e5d2d1a
NicoTimothy11 <nico.timothy319@gmail.com>
2023-08-27 22:44:13 +0800

    Set up for a new practice project

---
+++ BankAcc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Account {
+public:
+};
+
+class SavingsAcct : public Account {};
+class CheckingAcct : public Account {};
+
+int main() {}
