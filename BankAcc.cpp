094b152a2fc4b4aa52126fbaa9af3f28429bf350
NicoTimothy11 <nico.timothy319@gmail.com>
2023-08-29 13:44:01 +0800

    New changes

--- BankAcc.cpp
+++ BankAcc.cpp
@@ -5,12 +5,19 @@ using namespace std;
 class Account {
 public:
 	virtual void VirtualFunc() = 0;
+
+	virtual void ShowInfo() {
+		cout << "This account is private" << endl;
+	}
 };
 
 class Savings : public Account {
 public:
 	void VirtualFunc() override {
 		cout << "Withdraw" << endl;
+	}
+	void ShowInfo() override {
+		cout << "Savings Account Name: " << endl;
 	}
 };
 
@@ -19,6 +26,9 @@ public:
 	void VirtualFunc() override {
 		cout << "Chequess" << endl;
 	}
+	void ShowInfo() override {
+		cout << "Checkings Account Name: " << endl;
+	}
 };
 
 int main() {
@@ -26,6 +36,7 @@ int main() {
 
 	Savings* SavePtr = &Nico;
 	SavePtr -> VirtualFunc();
+	SavePtr -> ShowInfo();
 	return 0;
 }
 
